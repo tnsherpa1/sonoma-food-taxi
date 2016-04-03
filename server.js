f76432b0e147e18b9ab1f1bc50a66fc2586010a2
tnsherpa1 <tnsherpa1@gmail.com>
2016-04-03 09:24:41 -0700

    adds js cdn and custom scritps,styles in index

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var hbs= require('hbs');
 
+//require and load dotenv
+require('dotenv').load();
+
 //configure body-parser for receiving form data
 app.use(bodyparser.urlEncoded({ extended: true }));
 app.use(bodyparser.json());
@@ -21,9 +24,22 @@ mongoose.connect('mongodb://localhost/sonoma_food_taxi');
 var Customer = require('./models/customer');
 
 ///////API////ROUTES///////
-app.get('/')
+app.get('/api/customers', auth.ensureAuthenticated, function (req,res){
+	Customer.findById(req.customer, function( err,customer ) {
+		res.send();
+	});
+});
 
+/*
+* Catch all routes
+*/
+app.get('*', function(req,res){
+	res.render('index');
+});
 
+/*
+* Listen on localhost: 3000
+*/
 app.listen(3000, function(){
 	console.log("Server started at ", 3000);
 });
