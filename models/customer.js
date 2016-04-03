24946860eeb45e9168e2a8c7eb26308ee00f0502
tnsherpa1 <tnsherpa1@gmail.com>
2016-04-02 20:11:35 -0700

    creates customer model and password hash

--- models/customer.js
+++ models/customer.js
@@ -3,6 +3,37 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcryptjs');
 
 var customerSchema = new Schema({
-
-
+created: { type: Date },
+updated: { type: Date },
+name: {type: String},
+email: { type: String, unique: true, lowercase: true},
+password: { type: String, select: false}
 });
+
+
+customerSchema.pre('save', function(next) {
+	now = new Date();
+	this.updated = now;
+	if (!this.created) {
+		this.created = now;
+	}
+//encrypt password
+var customer = this;
+if (!customer.isModified('password')) {
+	return next();
+}
+bcrypt.genSalt(10, function(err,salt) {
+		bcrypt.hash(customer.password, salt, function(err, hash) {
+			customer.password = hash;
+			next();
+		});
+	});
+});
+customerSchema.methods.comparePassword = function ( password, done ) {
+	bcrypt.compare(password, this.password, function (err, isMatch){
+		done( err, isMatch );
+	});
+};
+
+var Customer = mongoose.model('Customer', customerSchema);
+module.exports = Customer;
