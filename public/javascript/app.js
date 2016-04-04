9d6a5385ebd111bc6ede8cc26524e46ab678cba9
tnsherpa1 <tnsherpa1@gmail.com>
2016-04-03 19:39:12 -0700

    adds angular services

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -96,13 +96,14 @@ LoginCtrl.$inject = ["$location", "Account"];
 function LoginCtrl($location, Account) {
 	var vm = this;
 	vm.new_customer = {}; //form_data
+	
 	vm.login = function() {
 		Account
-		.login(vm.new_customer)
-		.then(function() {
-			vm.new_customer = {};
-			$location.path('/profile');
-		});
+			.login(vm.new_customer)
+			.then(function() {
+				vm.new_customer = {};
+				$location.path('/profile');
+			});
 	};
 }
 
@@ -129,6 +130,112 @@ function logoutCtrl($location, Account) {
 			$location.path('/login');
 	});
 }
+
+///////////////////////////////////////////////////////////////////////////////
+////SERVICES////////SERVICES/////////SERVICES////////SERVICES/////SERVICES////
+/////////////////////////////////////////////////////////////////////////////
+Account.$inject = ["$http", "$q", "$auth"];
+function Account($http, $q, $auth) {
+	var self = this;
+	self.customer = null;
+
+	self.signup = signup;
+	self.login = login;
+	self.logout = logout;
+	self.currentCustomer = currentCustomer;
+	self.getProfile = getProfile;
+	self.updateProfile = updateProfile;
+
+	function signup(customerData) {
+		return (
+			$auth
+				.signup(customerData)
+				.then (
+					function onSuccess(response) {
+						$auth.setToken(response.data.token);
+					},
+					function onError(error) {
+						console.error(error);
+					}
+				)
+			);
+	}
+	function login(customerData) {
+		return (
+			$auth
+				.login(customerData)
+				.then ( 
+					function onSuccess(response) {
+					$auth.setToken(response.data.token);
+				},
+					function onError(error) {
+						console.error(error);
+					}
+				)
+			);
+	}
+	function logout() {
+		return ( 
+			$auth
+				.logout()
+				.then(function () {
+					self.customer = null;
+				})
+			);
+	}
+	function currentCustomer() {
+		if ( self.customer ) { return self.customer; }
+		if ( !auth.isAuthenticated() ) { return null; }
+
+		var deferred = $q.defer();
+		getProfile().then(
+			function onSuccess(response) {
+				self.customer = response.data;
+				deferred.resolve(self.customer);
+			},
+			function onError() {
+				$auth.logout();
+				self.customer = null;
+				deferred.reject();
+			}
+		)
+		self.customer = promise = deferred.promise;
+		return promise;
+	}
+	function getProfile() {
+		return $http.get('/api/me');
+	}
+
+	function updateProfile(profileData) {
+		return (
+			$http
+				.put('/api/me', profileData)
+				.then(
+					function (response) {
+						self.customer = response.data;
+					})
+		);
+	}
+
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
